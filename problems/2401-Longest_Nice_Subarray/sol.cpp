2962ca0db88117da9ee7af0f9f9aa81a482f4355
ARui <mail@arui.dev>
2025-03-19 03:43:47 +0800

    Solve 2401

--- problems/2401-Longest_Nice_Subarray/sol.cpp
+++ problems/2401-Longest_Nice_Subarray/sol.cpp
@@ -4,4 +4,24 @@
  * Runtime: 0 ms (100.00%)
  */
 
-2401. Longest Nice Subarray
+class Solution {
+public:
+    int longestNiceSubarray(vector<int>& nums) {
+        int cur = nums[0];
+        int tail = 0;
+        int curLen = 1;
+        int res = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            curLen++;
+            while ((cur & nums[i]) != 0 && curLen > 1) {
+                cur &= ~nums[tail++];
+                curLen--;
+            }
+            cur |= nums[i];
+
+            res = max(res, curLen);
+        }
+
+        return res;
+    }
+};
