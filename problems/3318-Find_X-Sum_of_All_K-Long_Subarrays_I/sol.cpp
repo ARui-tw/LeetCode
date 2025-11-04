5bfe3c2febf8c2e9a7963096e9cfe5d3037f2b11
ARui <mail@arui.dev>
2025-11-05 00:28:45 +0800

    Solve 3318

--- problems/3318-Find_X-Sum_of_All_K-Long_Subarrays_I/sol.cpp
+++ problems/3318-Find_X-Sum_of_All_K-Long_Subarrays_I/sol.cpp
@@ -4,4 +4,42 @@
  * Runtime: 9 ms (82.11%)
  */
 
-3318. Find X-Sum of All K-Long Subarrays I
+class Solution {
+public:
+    vector<int> findXSum(vector<int>& nums, int k, int x) {
+        unordered_map<int, int> um;
+        int n = nums.size();
+        vector<int> res;
+        vector<pair<int, int>> vec;
+
+        auto xSum = [&]() -> int {
+            for (auto [num, count] : um) {
+                vec.push_back({count, num});
+            }
+            sort(vec.begin(), vec.end());
+            int r = 0, need = x;
+            while (need-- && vec.size()) {
+                r += vec[vec.size() - 1].first * vec[vec.size() - 1].second;
+                vec.pop_back();
+            }
+            vec.clear();
+
+            return r;
+        };
+
+        for (int i = 0; i < k - 1; i++) {
+            um[nums[i]]++;
+        }
+
+        for (int i = k - 1; i < n; i++) {
+            if (i - k >= 0) {
+                um[nums[i - k]]--;
+            }
+            um[nums[i]]++;
+
+            res.push_back(xSum());
+        }
+
+        return res;
+    }
+};
