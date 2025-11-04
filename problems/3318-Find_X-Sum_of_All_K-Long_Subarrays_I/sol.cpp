37336d374e62c334661a576cf4c5253e20cfa103
ARui <mail@arui.dev>
2025-11-05 00:28:12 +0800

    Solve 3318

---
+++ problems/3318-Find_X-Sum_of_All_K-Long_Subarrays_I/sol.cpp
@@ -0,0 +1,7 @@
+/**
+ * Author: ARui<mail@arui.dev>
+ * Problem: https://leetcode.com/problems/find-x-sum-of-all-k-long-subarrays-i
+ * Runtime: 9 ms (82.11%)
+ */
+
+3318. Find X-Sum of All K-Long Subarrays I
