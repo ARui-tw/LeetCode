e811552c1a76c12206f7a0a04d09c30de5241157
ARui <mail@arui.dev>
2025-12-11 00:18:10 +0800

    Solve 3577

---
+++ problems/3577-Count_the_Number_of_Computer_Unlocking_Permutations/sol.cpp
@@ -0,0 +1,7 @@
+/**
+ * Author: ARui<mail@arui.dev>
+ * Problem: https://leetcode.com/problems/count-the-number-of-computer-unlocking-permutations
+ * Runtime: 0 ms (100.00%)
+ */
+
+3577. Count the Number of Computer Unlocking Permutations
