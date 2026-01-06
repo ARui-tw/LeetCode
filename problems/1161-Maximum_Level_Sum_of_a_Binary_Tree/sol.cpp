b4cc877b2623dfcd40bbbc7fcc26e7d155c23841
ARui <mail@arui.dev>
2026-01-06 23:25:20 +0800

    Solve 1161

---
+++ problems/1161-Maximum_Level_Sum_of_a_Binary_Tree/sol.cpp
@@ -0,0 +1,7 @@
+/**
+ * Author: ARui<mail@arui.dev>
+ * Problem: https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree
+ * Runtime: 3 ms (68.92%)
+ */
+
+1161. Maximum Level Sum of a Binary Tree
