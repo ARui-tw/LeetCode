820c8a415251999ff5c8907bf4e87510cf3b6637
ARui <mail@arui.dev>
2026-01-21 01:13:00 +0800

    Solve 3314

---
+++ problems/3314-Construct_the_Minimum_Bitwise_Array_I/sol.cpp
@@ -0,0 +1,7 @@
+/**
+ * Author: ARui<mail@arui.dev>
+ * Problem: https://leetcode.com/problems/construct-the-minimum-bitwise-array-i
+ * Runtime: 0 ms (100.00%)
+ */
+
+3314. Construct the Minimum Bitwise Array I
