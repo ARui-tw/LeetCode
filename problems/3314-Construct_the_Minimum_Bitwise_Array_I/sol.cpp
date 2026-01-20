3e96653243c94962cdfc76b8ceceb7fbe7421134
ARui <mail@arui.dev>
2026-01-21 01:13:16 +0800

    Solve 3314

--- problems/3314-Construct_the_Minimum_Bitwise_Array_I/sol.cpp
+++ problems/3314-Construct_the_Minimum_Bitwise_Array_I/sol.cpp
@@ -4,4 +4,34 @@
  * Runtime: 0 ms (100.00%)
  */
 
-3314. Construct the Minimum Bitwise Array I
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int> res;
+        res.reserve(nums.size());
+
+        for (auto& n : nums) {
+            if (n == 2) {
+                res.push_back(-1);
+                continue;
+            }
+            bitset<10> bs = n;
+            for (int i = 1; i < 10; i++) {
+                if (bs[i] == 0) {
+                    bs[i-1] = 0;
+                    break;
+                }
+            }
+            res.push_back(bs.to_ulong());
+        }
+
+        return res;
+    }
+};
+
+// 11 -> 1 or 10
+// 101 -> 100 or 101
+// 111 -> 11 or 100
+
+// 1011 -> 1001 or 1010
+// 1101 -> 1100
