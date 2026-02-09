bc4f2727d69fd516ddf83a948e18bd2ab3d65bb3
ARui <mail@arui.dev>
2026-02-09 23:02:11 +0800

    Solve 0621

--- problems/0621-Task_Scheduler/sol.cpp
+++ problems/0621-Task_Scheduler/sol.cpp
@@ -1,56 +1,26 @@
 /**
  * Author: ARui<mail@arui.dev>
  * Problem: https://leetcode.com/problems/task-scheduler
- * Runtime: 22 ms (49.06%)
+ * Runtime: 0 ms (100.00%)
  */
-
-const static auto _ = []() {
-  cin.tie(nullptr)->sync_with_stdio(false);
-  return nullptr;
-}();
 
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        priority_queue<pair<int, int>> ready;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> cooling;
+        int ma = -1, maCnt = 0;
         vector<int> vec(26, 0);
 
-        for (auto& c : tasks) {
-            vec[c - 'A']++;
-        }
-
-        for (int i = 0; i < 26; i++) {
-            if (vec[i])
-                ready.push({vec[i], i});
-        }
-
-        int res = 0;
-        while (true) {
-            if (ready.empty() && cooling.empty()) return res;
-
-            res++;
-
-            while (!cooling.empty()) {
-                auto [time, task] = cooling.top();
-                if (time < res) {
-                    ready.push({vec[task], task});
-                    cooling.pop();
-                } else break;
-            }
-
-            if (ready.empty()) {
-                continue;
-            }
-
-            auto [cnt, task] = ready.top();
-            ready.pop();
-            vec[task]--;
-            if (vec[task]) {
-                cooling.push({res + n, task});
+        for (auto& task : tasks) {
+            int& v = vec[task - 'A'];
+            v++;
+            if (v > ma) {
+                ma = v;
+                maCnt = 1;
+            } else if (v >= ma) {
+                maCnt++;
             }
         }
 
-        return res;
+        return max((int)tasks.size(), (n + 1) * (ma - 1) + (maCnt));
     }
 };
